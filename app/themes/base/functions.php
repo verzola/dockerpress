ab09db4a1d004accdd00852f6b968562b809c835
Gustavo Verzola <verzola@gmail.com>
2018-11-28 01:40:21 -0200

    add jquery as dependency in enqueue script and use document ready tot est it

--- app/themes/base/functions.php
+++ app/themes/base/functions.php
@@ -38,7 +38,7 @@ add_action('wp_enqueue_scripts', function () {
             wp_enqueue_script(
                 $name,
                 get_template_directory_uri() . '/dist/' . $fullName,
-                [],
+                ['jquery'],
                 null,
                 true
             );
--- app/themes/base/src/js/index.js
+++ app/themes/base/src/js/index.js
@@ -1,3 +1,4 @@
 import "../scss/main.scss";
+import $ from "jquery";
 
-console.log("hello world");
+$(document).ready(() => console.log("hello world"));
