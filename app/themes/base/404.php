ec71efaf96b63c6826bb6ad653898b4fce026494
Marcia Ibanez <marcia.ibanez.1@gmail.com>
2018-11-28 13:52:40 -0200

    style 404 page

--- app/themes/base/404.php
+++ app/themes/base/404.php
@@ -1,3 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404 - Not Found</title>
+    <?php wp_head() ?>
+</head>
+<body class="page-not-found">
 
-echo '404';
+    <h1>404</h1>
+    <h2>Not found <span>:(</span></h2>
+    <p>Sorry, but the page you are trying to view does not exist.</p>
+
+    <?php wp_footer() ?>
+</body>
+</html>
