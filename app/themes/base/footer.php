b10626375155508fc2a40d405d903bc896b141a3
Gustavo Verzola <verzola@gmail.com>
2018-12-20 15:02:23 -0200

    add footer

--- app/themes/base/footer.php
+++ app/themes/base/footer.php
@@ -1,5 +1,16 @@
-    <footer class="l-footer">
-    </footer>
+	<footer class="l-footer">
+		<div class="container">
+            <h3 class="l-footer__brand">
+                <?php echo bloginfo('name') ?>
+            </h3>
+
+			<div class="l-footer__social">
+				<a href="https://github.com/verzola/dockerpress">
+					<i class="fab fa-github"></i>
+				</a>
+			</div>
+		</div>
+	</footer>
 
     <?php wp_footer() ?>
 </body>
