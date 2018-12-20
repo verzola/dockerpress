f6e973be720a462df675f4fcb2ac4d4e8000ddb4
Gustavo Verzola <verzola@gmail.com>
2018-12-20 15:02:56 -0200

    improve card

--- app/themes/base/index.php
+++ app/themes/base/index.php
@@ -3,30 +3,32 @@
 <main class="l-main" role="main">
 
     <header class="c-hero">
-        <h1><i class="fas fa-pencil-alt"></i> Posts</h1>
+        <h1><i class="fas fa-newspaper"></i> Posts</h1>
     </header>
 
     <div class="container c-grid">
 
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post() ?>
 
-            <article class="c-article-card">
-                <?php the_post_thumbnail('medium', ['class' => 'c-article-card__image']) ?>
+            <article class="c-card c-grid__item">
+                <div class="c-card__image">
+                    <?php the_post_thumbnail('medium', ['class' => 'c-card__image']) ?>
 
-                <div class="c-article-card__content">
+                    <div class="c-author clearfix">
+                        <img class="c-author__avatar" src="<?php echo get_avatar_url(get_the_author_meta('ID')) ?>" />
+
+                        <div class="c-author__author"><?php the_author() ?></div>
+
+                        <time class="c-card__time"><?php the_time( get_option( 'date_format' ) ) ?></time>
+                    </div>
+                </div>
+
+                <div class="c-card__content">
                     <header>
-                        <div class="c-author clearfix">
-                            <img class="c-author__avatar" src="<?php echo get_avatar_url(get_the_author_meta('ID')) ?>" />
-
-                            <div class="c-author__author"><?php the_author() ?></div>
-
-                            <time class="c-article-card__time"><?php the_time( get_option( 'date_format' ) ) ?></time>
-                        </div>
-
-                        <h1 class="c-article-card__title"><?php the_title() ?></h1>
+                        <h1 class="c-card__title"><?php the_title() ?></h1>
                     </header>
 
-                    <div class="c-article-card__excerpt"><?php the_excerpt() ?></div>
+                    <div class="c-card__excerpt"><?php the_excerpt() ?></div>
 
                     <a class="c-button" href="<?php echo get_permalink() ?>">Read more</a>
                 </div>
