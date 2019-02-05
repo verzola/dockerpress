53a00f6512d5ba0814d45322c6cc46010b2ed4dd
Gustavo Verzola <verzola@gmail.com>
2019-02-05 20:28:51 -0200

    replace embed base theme with wp-base-theme

--- app/themes/base/src/js/index.js
+++
@@ -1,8 +0,0 @@
-import "../scss/main.scss";
-import "@fortawesome/fontawesome-free/js/all.js";
-import $ from "jquery";
-
-$(".js-hamburger").click(function() {
-  $(this).toggleClass("is-active");
-  $(".c-offcanvas").toggleClass("c-offcanvas--open");
-});
